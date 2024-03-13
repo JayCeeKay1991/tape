8614125bf0c21da74150765fee34f1bf6516e1aa
JayCeeKay1991 <johanna.kveton@gmail.com>
2024-03-13 17:38:12 +0100

    setup: package json

--- server/index.js
+++ server/index.js
@@ -3,4 +3,8 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-app.listen(process.env.PORT || 4000, () => console.log(`Server listening on port ${process.env.PORT || 4000}`));
+app.listen(process.env.PORT || 4000, (req, res) => {
+  console.log(`Server listening on port ${process.env.PORT || 4000}`);
+  res.send('Hello whatever')
+
+});
