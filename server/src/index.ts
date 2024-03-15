6786dce76a72d6b92b5c64d4d4334573157b9c06
Asiye Duman <asiyedumann@gmail.com>
2024-03-15 12:31:08 +0100

    typescript and jest configuration finished

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,9 +4,8 @@ const app = express();
 app.use(express.json());
 
 app.use('/', (req, res) => {
-  res.send('Haaaayy')
-})
+  res.send('Haaaayy');
+});
 app.listen(process.env.PORT || 4000, () => {
   console.log(`Server listening on port ${process.env.PORT || 4000}`);
-
 });
