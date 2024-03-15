7b55dd28c63b6ab1c0d220a997edc252a13144fd
JayCeeKay1991 <johanna.kveton@gmail.com>
2024-03-15 16:14:39 +0100

    feat: database connection and router

--- server/src/models/group.ts
+++ server/src/models/group.ts
@@ -1,29 +1,30 @@
-import { InferSchemaType, Schema, model } from "mongoose";
+import { InferSchemaType } from "mongoose";
+import mongoose from ".";
 
 export type groupType = InferSchemaType<typeof Group>;
 
 // defining data structure
-const Group = new Schema({
+const Group = new mongoose.Schema({
   name: String,
   picture: String,
   owner: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: "users",
   },
   members: [
     {
-      type: Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: "users",
     },
   ],
   mixTapes: [
     {
-      type: Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: "mixTapes",
     },
   ],
 });
 
-const GroupModel = model("groups", Group);
+const GroupModel = mongoose.model("groups", Group);
 
 export default GroupModel;
--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,28 +1,29 @@
-import { InferSchemaType, Schema, model } from "mongoose";
+import { InferSchemaType } from "mongoose";
+import mongoose from ".";
 
 export type userType = InferSchemaType<typeof User>;
 
 // defining data structure
-const User = new Schema({
+const User = new mongoose.Schema({
   userName: String,
   email: String,
   password: String,
   profilePic: String,
   groups: [
     {
-      type: Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: "groups",
       required: false,
     },
   ],
   mixTapes: [
     {
-      type: Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: "mixTapes",
     },
   ],
 });
 
-const UserModel = model("users", User);
+const UserModel = mongoose.model("users", User);
 
 export default UserModel;
