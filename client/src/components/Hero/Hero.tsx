e4fbbf09d1715cc1d4d22ae9871bef8bac1ff2cf
JayCeeKay1991 <johanna.kveton@gmail.com>
2024-03-16 11:05:03 +0100

    feat: added tests

---
+++ client/src/components/Hero/Hero.tsx
@@ -0,0 +1,16 @@
+
+const Hero = () => {
+
+  return (
+    <div id="hero-wrap">
+      <img id="hero-background" ></img>
+      <div  id="hero-body" >
+        <h1>Make mixtape streams with your friends</h1>
+        <p>Tape FM lets you upload mixes and share them in groups of your friends </p>
+        <button>Join now</button>
+      </div>
+    </div>
+  )
+}
+
+export default Hero;
---
+++ client/src/routes/Home/Home.tsx
@@ -0,0 +1,16 @@
+//import Nav from '../../components/Nav/Nav';
+import Hero from '../../components/Hero/Hero';
+import Features from '../../components/Features/Features';
+
+
+const Home = () => {
+
+  return (
+    <>
+    <Hero></Hero>
+    <Features></Features>
+    </>
+  )
+}
+
+export default Home;
