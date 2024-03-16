d87b1b9b99bc971b4d4b2ad3f63ea4702d39cd92
JayCeeKay1991 <johanna.kveton@gmail.com>
2024-03-16 12:46:45 +0100

    feat: channelList and channelItem

---
+++ client/src/components/ChannelItem/ChannelItem.tsx
@@ -0,0 +1,14 @@
+
+type ChannelItemProps {
+  channel:
+}
+
+const ChannelItem = ({channel}:ChannelItemProps) => {
+
+  return (
+      <div>This is a channel</div>
+  )
+
+}
+
+export default ChannelItem;
---
+++ client/src/components/ChannelList/ChannelList.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+//import AppContext from '../Context/Context';
+import ChannelItem from '../ChannelItem/ChannelItem';
+import getChannels from '../../'
+
+const ChannelList = () => {
+  const { user } = useAppContext();
+  const [channelList, setChannelList] = useState([]);
+
+  useEffect(() => {
+    const fetchAndSet = async () => {
+      const channelsPerUser = await getChannels(user._id)
+
+      setChannelList(channelsPerUser);
+    };
+    fetchAndSet();
+  }, [])
+
+  return (
+      channelList.length ? channelList.map(channel => <ChannelItem channel={channel}/>) : <p>No channels yet.</p>
+  )
+
+}
+
+export default ChannelList;
