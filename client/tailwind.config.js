7645476821bc09d150fa3d3e56ec027aa0f34321
Alastair Fraser <alastairfraser.work@gmail.com>
2024-03-16 16:46:03 +0100

    tailwind themes config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      tapeGreen: "#00A97C",
+      tapeOrange: "#FE7B27",
+      tapePink: "#EE87B5",
+      tapeBlack: "#151515",
+      tapeOffBlack: "#1B1B1B",
+    },
     extend: {},
   },
   plugins: [],
