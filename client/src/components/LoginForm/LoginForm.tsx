6be786084e0ed01943c5e4e511112bf856b271f5
JayCeeKay1991 <johanna.kveton@gmail.com>
2024-03-18 12:54:24 +0100

    fix: hotfix filenames

---
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+import { useMainContext, initialStateUser } from '../Context/Context';
+import { useNavigate } from 'react-router-dom';
+import { logIn } from '../../services/UserClientService';
+import './LoginForm.css';
+
+export type FormValuesUserLogin = {
+  email: string;
+  password: string;
+};
+
+const initialStateUserLogin = {
+  email: '',
+  password: '',
+};
+
+function LoginForm() {
+  const { user, setUser } = useMainContext();
+  const navigate = useNavigate();
+
+  const [formValuesUserLogin, setFormValuesUserLogin] =
+    useState<FormValuesUserLogin>(initialStateUserLogin);
+
+  // handler functions
+  function changeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setFormValuesUserLogin({ ...formValuesUserLogin, [name]: value });
+  }
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { email, password } = formValuesUserLogin;
+    const loginData = { email, password };
+
+    // make service call login function
+    const loggedInUser = await logIn(loginData);
+
+    // set user to the logged in user
+    loggedInUser && setUser(loggedInUser);
+
+    // empty the form
+    setFormValuesUserLogin(initialStateUserLogin);
+
+    // set the localstorage to the logged in user id
+    if (loggedInUser) {
+      localStorage.setItem('loggedInUser', loggedInUser._id);
+    }
+
+    // SET navigation to Dashboard
+    navigate('/dash');
+  };
+
+  // logout function redirects to homepage, will be moved to nav bar
+
+  const handleLogout = async () => {
+    setUser(initialStateUser);
+    setFormValuesUserLogin(initialStateUserLogin);
+    localStorage.clear();
+    //SET navigation to Homepage("/")
+    navigate('/home');
+  };
+
+  return (
+    <>
+      <form id="login-form" onSubmit={handleLogin}>
+        <input
+          name="email"
+          type="text"
+          value={formValuesUserLogin.email}
+          onChange={changeHandler}
+          placeholder="email"
+          required={true}
+        ></input>
+        <input
+          name="password"
+          type="password"
+          value={formValuesUserLogin.password}
+          onChange={changeHandler}
+          placeholder="password"
+          required={true}
+        ></input>
+        <button className="login-button" type="submit">
+          login
+        </button>
+      </form>
+    </>
+  )
+}
+
+export default LoginForm;
