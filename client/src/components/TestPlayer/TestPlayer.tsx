273e378a09b98e384ba1a8899f5c2cab091a5222
asiye duman <asiyeduman@MacBook-Pro-von-asiye.local>
2024-03-18 17:25:33 +0100

    feat:player previous and next functionalities

--- client/src/components/TestPlayer/TestPlayer.tsx
+++ client/src/components/TestPlayer/TestPlayer.tsx
@@ -1,23 +1,22 @@
-import { MouseEvent, useEffect, useState, useRef} from "react";
-import { Howl } from "howler";
+import { MouseEvent, useEffect, useState, useRef } from 'react';
+import { Howl } from 'howler';
 
 /* So actually the main error I'm getting with this is that the html5 audiopool is exhausted, and just found a stack overflow thread that talks
 about this error with Howler and suggested the solution is to do it using the native html audio tag, so could maybe try that unless someone else finds a solution */
 
-const testUrl1 = 'https://res.cloudinary.com/ddj3xjxrc/video/upload/v1710529539/D.J._Poizen_Visits_Kool_Kyle_Side_A_ncjkhb.mp3'
-const testUrl2 = `https://res.cloudinary.com/ddj3xjxrc/video/upload/v1710529445/podcast_mark-mendoza-podcast_1988-mixtape_1000413811719_ts9qep.mp3`
-
+const testUrl1 =
+  'https://res.cloudinary.com/ddj3xjxrc/video/upload/v1710529539/D.J._Poizen_Visits_Kool_Kyle_Side_A_ncjkhb.mp3';
+const testUrl2 = `https://res.cloudinary.com/ddj3xjxrc/video/upload/v1710529445/podcast_mark-mendoza-podcast_1988-mixtape_1000413811719_ts9qep.mp3`;
 
 // Define the component
-const TestPlayer  = () => {
+const TestPlayer = () => {
   const channelMixtapes = [testUrl1, testUrl2]; // urls of audio files
-  const [stream, setStream] = useState<Howl[]>([]) // current stream, array of howls created in useeffect
-  const [streamIndex, setStreamIndex] = useState<number>(0) // stores the index of current mixTape in stream
-  const [muted, setMuted] = useState<boolean>(false) // flag for if player is muted
-  const durationRef = useRef<HTMLParagraphElement>(null) // ref to duration p element that will change
-  const totalDurationRef = useRef<HTMLParagraphElement>(null) // ref to duration p that will show mixtapes total length
-  const volumeRef = useRef<HTMLParagraphElement>(null) // ref to volume p that will show current vol
-
+  const [stream, setStream] = useState<Howl[]>([]); // current stream, array of howls created in useeffect
+  const [streamIndex, setStreamIndex] = useState<number>(0); // stores the index of current mixTape in stream
+  const [muted, setMuted] = useState<boolean>(false); // flag for if player is muted
+  const durationRef = useRef<HTMLParagraphElement>(null); // ref to duration p element that will change
+  const totalDurationRef = useRef<HTMLParagraphElement>(null); // ref to duration p that will show mixtapes total length
+  const volumeRef = useRef<HTMLParagraphElement>(null); // ref to volume p that will show current vol
 
   useEffect(() => {
     // create stream array from mixTapes
@@ -27,27 +26,32 @@ const TestPlayer  = () => {
         return new Howl({
           src: [mixtape],
           html5: true, // html 5 is better for large files
-          onplay: function (this: Howl) { // defines callback function that runs onplay, must be function needs explicit this
+          onplay: function (this: Howl) {
+            // defines callback function that runs onplay, must be function needs explicit this
             if (totalDurationRef.current) {
               // renders total duration in p element
-              totalDurationRef.current.textContent = formatTime(Math.round(this.duration()));
+              totalDurationRef.current.textContent = formatTime(
+                Math.round(this.duration())
+              );
             }
             if (volumeRef.current) {
               // renders default 100% volume
-              volumeRef.current.textContent = (this.volume() * 100).toString()
+              volumeRef.current.textContent = (this.volume() * 100).toString();
             }
             const timerId = setInterval(() => {
               // handles the rendering of the currently elapsed time by updating every second
               if (this.playing()) {
                 const currentTime = this.seek();
-                // seek is property of howl, finds current point 
+                // seek is property of howl, finds current point
                 if (durationRef.current) {
-                  durationRef.current.textContent = formatTime(Math.round(currentTime));
+                  durationRef.current.textContent = formatTime(
+                    Math.round(currentTime)
+                  );
                 }
               }
             }, 1000);
             return () => clearInterval(timerId);
-          }
+          },
         });
       });
       return mixtapes;
@@ -56,102 +60,162 @@ const TestPlayer  = () => {
     // set the state to the stream produced by above function
     setStream(generatedStream);
   }, []);
-  
+
   // just to examine properties etc
-  console.log(stream);
+  // console.log(stream);
 
   // this all needs refactoring, was more to test and illustrate functionality, not DRY
   const handlePlayClick = (event: MouseEvent<HTMLButtonElement>) => {
-    console.log('play clicked')
+    console.log('play clicked');
     const currentMixtape = stream[streamIndex];
     if (!currentMixtape.playing()) {
-      currentMixtape.play()
+      currentMixtape.play();
     }
-  }
+  };
 
   const handlePauseClick = (event: MouseEvent<HTMLButtonElement>) => {
-    console.log('pause clicked')
+    console.log('pause clicked');
     const currentMixtape = stream[streamIndex];
     if (currentMixtape.playing()) {
-      currentMixtape.pause()
+      currentMixtape.pause();
     }
-  }
+  };
 
   const handleStopClick = (event: MouseEvent<HTMLButtonElement>) => {
     const currentMixtape = stream[streamIndex];
-    console.log('stop clicked')
+    console.log(currentMixtape);
+    console.log('stop clicked');
     if (currentMixtape.playing()) {
-      currentMixtape.stop()
+      currentMixtape.stop();
     }
-  }
+  };
 
   const handleNextClick = (event: MouseEvent<HTMLButtonElement>) => {
     console.log('next clicked');
     // tried to do this with a currentMixtape state but didnt work as well
     const currentMixtape = stream[streamIndex];
     currentMixtape.stop();
-    const newIndex = streamIndex + 1;
-    setStreamIndex(newIndex);
-    const newMixtape = stream[newIndex];
-    console.log;(newMixtape)
-    newMixtape.play();
+
+    // console.log(streamIndex);
+    if (streamIndex < stream.length - 1) {
+      const newIndex = streamIndex + 1;
+      setStreamIndex(newIndex);
+
+      const newMixtape = stream[newIndex];
+      newMixtape.play();
+    } else {
+      // else start from the beginning
+      const newIndex = 0;
+      setStreamIndex(newIndex);
+      const newMixtape = stream[newIndex];
+      newMixtape.play();
+    }
   };
 
-  // super ssimple time formatting util function
-  const formatTime = (seconds: number) => {
-    return Math.floor(seconds / 60) + ':' + ('0' + Math.floor(seconds % 60)).slice(-2);
-  };  
+  const handlePreviousClick = (event: MouseEvent<HTMLButtonElement>) => {
+    console.log('Previous clicked');
 
-  const handleVolumeUp = (event: MouseEvent<HTMLButtonElement>) => {  
+    // stop the current one
+    const currentMixtape = stream[streamIndex];
+    currentMixtape.stop();
+
+    //if streamIndex is greater or equal than 1, decrement it
+    if (streamIndex >= 1) {
+      const newIndex = streamIndex - 1;
+
+      setStreamIndex(newIndex);
+
+      const newMixTape = stream[newIndex];
+      newMixTape.play();
+      // else go to the end
+    } else {
+      const newIndex = stream.length - 1;
+      setStreamIndex(newIndex);
+
+      const newMixtape = stream[newIndex];
+      newMixtape.play();
+    }
+  };
+
+  // super simple time formatting util function
+  const formatTime = (seconds: number) => {
+    return (
+      Math.floor(seconds / 60) +
+      ':' +
+      ('0' + Math.floor(seconds % 60)).slice(-2)
+    );
+  };
+
+  const handleVolumeUp = (event: MouseEvent<HTMLButtonElement>) => {
     const currentMixtape = stream[streamIndex];
     const newVolume = Math.min(currentMixtape.volume() + 0.05, 1);
     currentMixtape.volume(newVolume);
     if (volumeRef.current) {
       // renders new volume
-      volumeRef.current.textContent = Math.round(currentMixtape.volume() * 100).toString();
+      volumeRef.current.textContent = Math.round(
+        currentMixtape.volume() * 100
+      ).toString();
     }
-  }
+  };
 
-  const handleVolumeDown = (event: MouseEvent<HTMLButtonElement>) => {  
+  const handleVolumeDown = (event: MouseEvent<HTMLButtonElement>) => {
     const currentMixtape = stream[streamIndex];
     const newVolume = Math.max(currentMixtape.volume() - 0.05, 0);
     currentMixtape.volume(newVolume);
     if (volumeRef.current) {
       // renders new volume
-      volumeRef.current.textContent = Math.round(currentMixtape.volume() * 100).toString();
+      volumeRef.current.textContent = Math.round(
+        currentMixtape.volume() * 100
+      ).toString();
     }
-  }
+  };
 
   const handleToggleMute = (event: MouseEvent<HTMLButtonElement>) => {
     const currentMixtape = stream[streamIndex];
     if (muted === true) {
-      currentMixtape.volume(1)
-      setMuted(false)
+      currentMixtape.volume(1);
+      setMuted(false);
+    } else {
+      currentMixtape.volume(0);
+      setMuted(true);
     }
-    else {
-      currentMixtape.volume(0)
-      setMuted(true)
-    }
-  }
-  
+  };
 
   return (
     <div className="player">
       <h1>Channel #1</h1>
       <div className="progress-bar">
-        <p ref={durationRef}></p><p ref={totalDurationRef}></p>
+        <p ref={durationRef}></p>
+        <p ref={totalDurationRef}></p>
       </div>
       <div className="player-controls">
-        <button type="button" onClick={handlePlayClick}>Play</button>
-        <button type="button" onClick={handlePauseClick}>Pause</button>
-        <button type="button" onClick={handleStopClick}>Stop</button>
-        <button type="button" onClick={handleNextClick}>Next</button>
+        <button type="button" onClick={handlePlayClick}>
+          Play
+        </button>
+        <button type="button" onClick={handlePauseClick}>
+          Pause
+        </button>
+        <button type="button" onClick={handleStopClick}>
+          Stop
+        </button>
+        <button type="button" onClick={handlePreviousClick}>
+          Previous
+        </button>
+        <button type="button" onClick={handleNextClick}>
+          Next
+        </button>
       </div>
       <div className="volume-controls">
         <p ref={volumeRef}></p>
-        <button type="button" onClick={handleToggleMute}>Mute</button>
-        <button type="button" onClick={handleVolumeUp}>Volume Up</button>
-        <button type="button" onClick={handleVolumeDown}>Volume Down</button>
+        <button type="button" onClick={handleToggleMute}>
+          Mute
+        </button>
+        <button type="button" onClick={handleVolumeUp}>
+          Volume Up
+        </button>
+        <button type="button" onClick={handleVolumeDown}>
+          Volume Down
+        </button>
       </div>
     </div>
   );
