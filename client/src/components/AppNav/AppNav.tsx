7b15ae76a8c6f7952e4eb653f8d54bd240e937fd
Joe Copplestone <strasseneck@gmail.com>
2024-03-18 19:59:44 +0100

    Feat: AppNav styled routes work

--- client/src/components/AppNav/AppNav.tsx
+++ client/src/components/AppNav/AppNav.tsx
@@ -6,7 +6,7 @@ import { MdHome } from "react-icons/md";
 import { FiLogOut } from "react-icons/fi";
 
 const AppNav = () => {
-    const { user, setUser } = useMainContext()
+    const { setUser } = useMainContext()
     const navigate = useNavigate();
 
     const handleLogout = async () => {
@@ -17,20 +17,22 @@ const AppNav = () => {
 
     return (
         <nav className="w-full h-20 bg-tapeBlack fixed top-0 left-0 flex flex-row items-center justify-between pr-5 pl-5">
-            <Link to={`/home`} >
-                <a className=" text-tapeWhite"><MdHome size={25} /></a>
-            </Link>
-            <input type='text' placeholder='Search...' />
-            <ul className="flex flex-row text-tapeWhite">
-                <Link to={'/user'}>
-                    <div className="overflow-hidden rounded-full w-[50px] h-[50px]">
-                        <img src={johnMartin} className='w-16 h-16 object-cover' style={{objectPosition: 'center-center'}}/>
-                    </div>
+            <div className="flex flex-row align-middle">
+                <Link to={`/home`} >
+                    <a className=" text-tapeWhite"><MdHome size={30} /></a>
                 </Link>
-                <li className="pr-5">
-                    <a className=" text-tapeWhite"onClick={handleLogout}><FiLogOut size={25}/></a>
-                </li>
-            </ul>
+                <input className="bg-tapeBlack text-tapeWhite mx-8 focus:outline none" type='text' placeholder='Search...' />
+            </div>     
+            <div className="flex flex-row text-tapeWhite">
+                    <Link to={'/user'}>
+                        <div className="overflow-hidden rounded-full w-[50px] h-[50px]">
+                            <img src={johnMartin} className='w-16 h-16 object-cover' style={{ objectPosition: 'center-center' }} />
+                        </div>
+                    </Link>
+                <div className="flex flex-row align-middle">
+                    <button className="bg-none text-tapeWhite mx-8" onClick={handleLogout}><FiLogOut size={30} /></button>
+                </div>
+            </div>
         </nav>
     );
 }
