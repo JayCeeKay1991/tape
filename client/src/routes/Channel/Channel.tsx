2ff52c57f7af53923246756f50dcbe2863a0c005
Alastair Fraser <alastairfraser.work@gmail.com>
2024-03-21 20:26:25 +0100

    feat: stream functionality

--- client/src/routes/Channel/Channel.tsx
+++ client/src/routes/Channel/Channel.tsx
@@ -1,26 +1,23 @@
-
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 // types
-import { User } from '@/types/User';
-import { ChannelType } from '@/types/Channel';
+import { User } from "@/types/User";
+import { ChannelType } from "@/types/Channel";
 // services
-import { getAllUsers } from '@/services/UserClientService';
+import { getAllUsers } from "@/services/UserClientService";
 import { getChannel } from "@/services/ChannelClientService";
 // components
-import TestPlayer from '@/components/TestPlayer/TestPlayer';
-import { useMainContext } from '@/components/Context/Context';
-import AddMembersSelect from '@/components/AddMembersSelect/AddMembersSelect';
-import AddMixtapeForm from '@/components/AddMixtapeForm/AddMixtapeForm';
+import TestPlayer from "@/components/TestPlayer/TestPlayer";
+import { useMainContext } from "@/components/Context/Context";
+import AddMembersSelect from "@/components/AddMembersSelect/AddMembersSelect";
+import AddMixtapeForm from "@/components/AddMixtapeForm/AddMixtapeForm";
 import CommentList from "@/components/CommentList/CommentList";
 // styling
 import { MdPlayArrow } from "react-icons/md";
+import { GoPlus } from "react-icons/go";
 // import AudioWave from "@/components/AudioWave/AudioWave";
 // utils
 import { extractStreamUrls } from "@/utils/extractStreamUrls";
-
-
-
 
 const Channel = () => {
   const { user } = useMainContext();
@@ -28,10 +25,11 @@ const Channel = () => {
   const [channel, setChannel] = useState<ChannelType>(location.state.channel);
   const [showMixForm, setShowMixForm] = useState(false);
   const [showMemberForm, setShowMemberForm] = useState(false);
+  const [isCommentsOpen, setIsCommentsOpen] = useState(true);
 
   const initialState = {
-    name: '',
-    url: '',
+    name: "",
+    url: "",
     duration: 0,
     creator: user,
     parentChannel: channel,
@@ -46,79 +44,151 @@ const Channel = () => {
       try {
         const allUsers = await getAllUsers();
         setUsers(allUsers);
-
       } catch (error) {
-        console.error('error getting all users');
+        console.error("error getting all users");
       }
     }
     async function retrieveChannel() {
       try {
-        const channel = await getChannel(initialState.parentChannel._id)
+        const channel = await getChannel(initialState.parentChannel._id);
         setChannel(channel);
       } catch (error) {
-        console.error('error getting channel');
-
+        console.error("error getting channel");
       }
     }
-    retrieveChannel()
+    retrieveChannel();
     retrieveAllUsers();
   }, []);
 
   // Generate stream from mixtape urls
-  const currentStream = extractStreamUrls(channel.mixTapes)
+  const currentStream = extractStreamUrls(channel.mixTapes);
 
   const toggleMixForm = () => {
     setShowMixForm(!showMixForm);
     setShowMemberForm(false);
-  };
+	};
+
   const toggleMemberForm = () => {
     setShowMemberForm(!showMemberForm);
     setShowMixForm(false);
   };
 
+	const toggleComments = () => {
+		if (isCommentsOpen === true) {
+			setIsCommentsOpen(false)
+		} else {
+			setIsCommentsOpen(true)
+		}
+	};
+
+	console.log(channel.members)
+
   return (
-
-    <div id="channel">
-      <div id="channel-element" className="text-tapeWhite h-72 bg-gradient-to-r from-tapePink to-tapeYellow flex justify-between p-10 w-11/12 m-12 rounded-2xl" >
-        <div id="channel-info" className="w-2/5 text-xl" >
-          <h1 className="flex items-stretch text-3xl font-bold my-3" ><MdPlayArrow size={35} />{channel.name}</h1>
-          <p className="pl-3" >{channel.mixTapes.length ? `${channel.mixTapes.length} mixtape${channel.mixTapes.length === 1 ? '' : 's'}` : 'No mixtapes'}</p>
-          <p className="pl-3">{channel.members.length ? `${channel.members.length} member${channel.members.length === 1 ? '' : 's'}` : 'No members'}</p>
-
+    <div id="channel" className="flex flex-col items-center">
+      <div
+        id="channel-element"
+        className="text-tapeWhite w-[1350px] h-72 flex justify-between p-10 rounded-2xl bg-gradient-to-r from-tapePink to-tapeYellow mb-[20px]"
+      >
+        <div id="channel-info" className="w-2/5 text-xl">
+          <div className="flex flex-row">
+            <MdPlayArrow size={70} />
+            <div className="flex flex-col">
+              <h1 className="text-[55px] font-medium mb-[20px]">
+                {channel.name}
+              </h1>
+              <div className="flex flex-row">
+                <p className="mr-[20px] font-medium">
+                  {channel.mixTapes.length
+                    ? `${channel.mixTapes.length} mixtape${
+                        channel.mixTapes.length === 1 ? "" : "s"
+                      }`
+                    : "0 mixtapes"}
+                </p>
+                <p className="font-medium">
+                  {channel.members.length
+                    ? `${channel.members.length} member${
+                        channel.members.length === 1 ? "" : "s"
+                      }`
+                    : "0 members"}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row ml-[30px] mt-[50px]">
+            {channel.members.map((member) => {
+              return (
+                <div
+                  key={member._id}
+                  className="w-[80px] h-[80px] overflow-hidden rounded-full border-tapePink border-[2px] -ml-[30px]"
+                >
+                  <img src={member.profilePic}></img>
+                </div>
+              );
+            })}
+            <button className="w-[80px] h-[80px] flex flex-row justify-center items-center bg-tapeBlack rounded-full border-tapePink border-[2px] -ml-[30px]" onClick={toggleMemberForm}>
+              <GoPlus className="text-tapeWhite" size={30} />
+            </button>
+          </div>
         </div>
-          {/* <AudioWave/> */}
+
+        {showMemberForm && (
+          <AddMembersSelect channel={channel} setChannel={setChannel} />
+        )}
+        {/* <AudioWave/> */}
         <img src={channel.picture} className="w-48 rounded-2xl object-cover" />
       </div>
-      <button onClick={toggleMixForm} className='white-button ml-12'>
-        Add Mixtape
-      </button>
-      <button onClick={toggleMemberForm} className='white-button'>
 
-        Add Members
-      </button>
-      {showMixForm ? (
+      <div className="w-full h-[100px] pl-[50px] pr-[50px] flex flex-col items-start">
+        <div className="flex flex-row">
+          {isCommentsOpen ? (
+            <>
+              <button
+                className="border-none mr-[40px] text-[20px] font-medium text-tapeWhite"
+                onClick={toggleComments}
+              >
+                Comments
+              </button>
+              <button
+                className="border-none mr-[40px] text-[20px] font-medium text-tapeDarkGrey"
+                onClick={toggleComments}
+              >
+                Uploads
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                className="border-none mr-[40px] text-[20px] text-tapeDarkGrey"
+                onClick={toggleComments}
+              >
+                Comments
+              </button>
+              <button
+                className="border-none mr-[40px] text-[20px] text-tapeWhite"
+                onClick={toggleComments}
+              >
+                Uploads
+              </button>
+            </>
+          )}
+        </div>
+        <hr className="w-full mt-[20px] border-tapeDarkGrey"></hr>
+      </div>
+
+      {isCommentsOpen ? (
+        <CommentList channel={channel} setChannel={setChannel} />
+      ) : (
         <AddMixtapeForm
-        channelId={channel._id}
+          channelId={channel._id}
           channel={channel}
           setChannel={setChannel}
           setShowMixForm={setShowMixForm}
         />
-      ) : (
-        <></>
       )}
-      {showMemberForm ? (
 
-        <AddMembersSelect channel={channel} setChannel={setChannel} />
-
-      ) : (
-        <></>
-      )}
       <TestPlayer urls={currentStream} />
-
-      <CommentList channel={channel} />
     </div>
   );
-
 };
 
 export default Channel;
