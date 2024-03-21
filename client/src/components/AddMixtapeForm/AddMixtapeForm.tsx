d7e959154dc9f816d35baa617cd3f3c0b40db12c
asiye duman <asiyeduman@MacBook-Pro-von-asiye.local>
2024-03-21 12:12:22 +0100

    Mixtape front and back typescript

--- client/src/components/AddMixtapeForm/AddMixtapeForm.tsx
+++ client/src/components/AddMixtapeForm/AddMixtapeForm.tsx
@@ -1,164 +1,204 @@
-import { useState, Dispatch, SetStateAction } from 'react'
-import { User } from '../../types/User'
-import { CloudinaryRes, postMusicToCloudinary } from "../../services/CloudinaryService";
-import { createMixTape } from "../../services/MixtapeClientService";
+import { useState, Dispatch, SetStateAction } from 'react';
+import {
+  CloudinaryRes,
+  postMusicToCloudinary,
+} from '../../services/CloudinaryService';
+import { createMixTape } from '../../services/MixtapeClientService';
 import { ChannelType } from '../../types/Channel';
-import { MixTape } from "../../types/Mixtape";
+import { useMainContext } from '../Context/Context';
 
-
-interface AddMixtapeFormProps {
-    channelId: string
-    user: User
-    setChannel: Dispatch<SetStateAction<ChannelType>>
-    setShowMixForm: Dispatch<SetStateAction<boolean>>
-}
-
-type FormValues = Omit<MixTape, '_id'>;
-
-
-
-const initCloudinaryResState = {
-    public_id: '',
-    version: 0,
-    signature: '',
-    width: 0,
-    height: 0,
-    format: '',
-    resource_type: '',
-    created_at: '',
-    tags: [],
-    bytes: 0,
-    type: '',
-    etag: '',
-    placeholder: false,
-    url: '',
-    secure_url: '',
-    duration: 0,
-  }
-
-
-const AddMixtapeForm = ({ channelId, user, setChannel, setShowMixForm }: AddMixtapeFormProps) => {
-
-    const initialState = {
-        name: '',
-        url: '',
-        duration: 0,
-        creator: user,
-        parentChannel: channel,
-        channels: [],
-        users: [],
-    };
-
-
-    const [file, setFile] = useState<File | null>(null);
-    const [uploading, setUploading] = useState(false);
-    const [uploadComplete, setUploadComplete] = useState(false);
-    const [cldResponse, setCldResponse] = useState<CloudinaryRes>(initCloudinaryResState);
-    const [formValues, setFormValues] = useState<FormValues>(initialState);
-
-
-    const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value, type, files } = e.target;
-        if (type === 'file' && files) {
-            setFile(files[0]); // Set the image file
-        } else setFormValues({ ...formValues, [name]: value });
-    };
-
-    // upload file to cloudinary
-    const uploadFile = async (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-
-        if (!file) {
-            console.error("Please select a file.");
-            return;
-        }
-        setUploading(true);
-        const uniqueUploadId = `uqid-${Date.now()}`;
-        const chunkSize = 5 * 1024 * 1024; // 5MB
-        const totalChunks = Math.ceil(file.size / chunkSize);
-        let currentChunk = 0;
-
-        const onProgress = () => {
-            currentChunk++;
-            console.log(`Upload progress: Chunk ${currentChunk} of ${totalChunks}`);
-            if (currentChunk < totalChunks) {
-                const nextStart = currentChunk * chunkSize;
-                const nextEnd = Math.min(nextStart + chunkSize, file.size);
-                uploadNextChunk(nextStart, nextEnd);
-            } else {
-                setUploadComplete(true);
-                setUploading(false);
-                console.info("File upload complete.");
-            }
-        };
-
-        const onError = (error: Error | string): void => {
-            if (error instanceof Error) console.error("Error uploading chunk:", error.message);
-            else if (typeof error === 'string') console.error("Error uploading chunk:", error);
-            else console.error("Error uploading chunk: An unknown error occurred");
-            setUploading(false);
-        };
-
-        const onComplete = (response: CloudinaryRes) => {
-            setCldResponse(response);
-            console.log("Upload complete response:", response);
-            setUploading(false);
-        };
-
-        const uploadNextChunk = (start: number, end: number) => {
-            postMusicToCloudinary(file, uniqueUploadId, start, end, onProgress, onError, onComplete);
-        };
-
-        uploadNextChunk(0, Math.min(chunkSize, file.size));
-    };
-
-
-    // add the image to database together with the other form values
-    const submitHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        const addMixtape = async () => {
-            try {
-                if (cldResponse) {
-                    const fileUrl = cldResponse.secure_url;
-                    const duration = cldResponse.duration;
-                    const newMixtapeData = {
-                        ...formValues,
-                        url: fileUrl,
-                        duration: duration,
-                        parentChannel: channel
-                    };
-                    const newMixTape = await createMixTape(newMixtapeData);
-                    setChannel({
-                        ...channel,
-                        mixTapes: [
-                            ...channel.mixTapes,
-                            newMixTape
-                        ]
-                    })
-                } else throw new Error('No uploaded file to add.');
-                setFormValues(initialState);
-                setFile(null);
-                setShowMixForm(false);
-            } catch (error) {
-                console.error(error)
-            }
-        };
-        addMixtape();
-    }
-
-    return (
-        <form className="text-tapeWhite flex flex-col w-72 gap-2" >
-            <input name="name" type="text" onChange={changeHandler} placeholder="mixtape name"></input>
-            <div className="flex" >
-                <input name="file" type="file" onChange={changeHandler} ></input>
-                <button className="white-button" onClick={uploadFile} disabled={uploading} >{uploading ? "Uploading..." : "Upload"}</button>
-            </div>
-            {
-                uploading ? <></> : <button className="white-button" onClick={submitHandler} >Add Mixtape</button>
-            }
-        </form>
-    )
-
+type AddMixtapeFormProps = {
+  channelId: string;
+  channel: ChannelType;
+  setChannel: Dispatch<SetStateAction<ChannelType>>;
+  setShowMixForm: Dispatch<SetStateAction<boolean>>;
 };
 
-export default AddMixtapeForm
+// Form for add mixtape
+type FormValuesMixTape = {
+  name: string;
+  file: File | null;
+};
+const initialStateFormValuesMixTape = {
+  name: '',
+  file: null,
+};
+
+const initCloudinaryResState = {
+  public_id: '',
+  version: 0,
+  signature: '',
+  width: 0,
+  height: 0,
+  format: '',
+  resource_type: '',
+  created_at: '',
+  tags: [],
+  bytes: 0,
+  type: '',
+  etag: '',
+  placeholder: false,
+  url: '',
+  secure_url: '',
+  duration: 0,
+};
+
+const AddMixtapeForm = ({
+  channelId,
+  channel,
+  setChannel,
+  setShowMixForm,
+}: AddMixtapeFormProps) => {
+  const [file, setFile] = useState<File | null>(null);
+  const [uploading, setUploading] = useState(false);
+  const [uploadComplete, setUploadComplete] = useState(false);
+  const [cldResponse, setCldResponse] = useState<CloudinaryRes>(
+    initCloudinaryResState
+  );
+
+  const [formValuesMixTape, setFormValuesMixTape] = useState<FormValuesMixTape>(
+    initialStateFormValuesMixTape
+  );
+
+  const { user } = useMainContext();
+  //Change handler function keeps track of the upload form
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type, files } = e.target;
+
+    if (type === 'file' && files) {
+      setFile(files[0]); // Set the image file
+    } else
+      setFormValuesMixTape((prevFormData) => ({
+        ...prevFormData,
+        [name]: value,
+      }));
+  };
+
+  // upload file to cloudinary
+  const uploadFile = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if (!file) {
+      console.error('Please select a file.');
+      return;
+    }
+    setUploading(true);
+    const uniqueUploadId = `uqid-${Date.now()}`;
+    const chunkSize = 5 * 1024 * 1024; // 5MB
+    const totalChunks = Math.ceil(file.size / chunkSize);
+    let currentChunk = 0;
+
+    const onProgress = () => {
+      currentChunk++;
+      console.log(`Upload progress: Chunk ${currentChunk} of ${totalChunks}`);
+      if (currentChunk < totalChunks) {
+        const nextStart = currentChunk * chunkSize;
+        const nextEnd = Math.min(nextStart + chunkSize, file.size);
+        uploadNextChunk(nextStart, nextEnd);
+      } else {
+        setUploadComplete(true);
+        setUploading(false);
+        console.info('File upload complete.');
+      }
+    };
+
+    const onError = (error: Error | string): void => {
+      if (error instanceof Error)
+        console.error('Error uploading chunk:', error.message);
+      else if (typeof error === 'string')
+        console.error('Error uploading chunk:', error);
+      else console.error('Error uploading chunk: An unknown error occurred');
+      setUploading(false);
+    };
+
+    const onComplete = (response: CloudinaryRes) => {
+      setCldResponse(response);
+      console.log('Upload complete response:', response);
+      setUploading(false);
+    };
+
+    const uploadNextChunk = (start: number, end: number) => {
+      postMusicToCloudinary(
+        file,
+        uniqueUploadId,
+        start,
+        end,
+        onProgress,
+        onError,
+        onComplete
+      );
+    };
+
+    uploadNextChunk(0, Math.min(chunkSize, file.size));
+  };
+
+  // add the image to database together with the other form values
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const addMixtape = async () => {
+      try {
+        if (cldResponse) {
+          const fileUrl = cldResponse.secure_url;
+          const duration = cldResponse.duration;
+          const { name, file } = formValuesMixTape;
+          const newMixtapeData = {
+            name,
+            url: fileUrl,
+            duration: duration,
+            channel: channelId,
+            creator: user._id,
+          };
+
+          const newMixTape = await createMixTape(newMixtapeData);
+
+          const updatedChannel = {
+            ...channel,
+            mixTapes: [...channel.mixTapes, newMixTape],
+          };
+          setChannel(updatedChannel);
+
+          setFormValuesMixTape(initialStateFormValuesMixTape);
+          setFile(null);
+          setShowMixForm(false);
+        } else throw new Error('No uploaded file to add.');
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    addMixtape();
+  };
+
+  return (
+    <form
+      className="text-tapeWhite flex flex-col w-72 gap-5 m-10"
+      onSubmit={submitHandler}
+    >
+      <input
+        name="name"
+        type="text"
+        value={formValuesMixTape.name}
+        onChange={changeHandler}
+        placeholder="mixtape name"
+      ></input>
+      <div className="flex flex-row gap-3">
+        <input name="file" type="file" onChange={changeHandler}></input>
+        <button
+          className="white-button"
+          onClick={uploadFile}
+          disabled={uploading}
+        >
+          {uploading ? 'Uploading...' : 'Upload'}
+        </button>
+      </div>
+      {uploading ? (
+        <></>
+      ) : (
+        <button type="submit" className="white-button">
+          Add Mixtape
+        </button>
+      )}
+    </form>
+  );
+};
+
+export default AddMixtapeForm;
--- client/src/routes/Channel/Channel.tsx
+++ client/src/routes/Channel/Channel.tsx
@@ -1,17 +1,17 @@
-import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 // types
-import { User } from "../../types/User";
-import { ChannelType } from "../../types/Channel";
+import { User } from '../../types/User';
+import { ChannelType } from '../../types/Channel';
 // services
-import { getAllUsers } from "../../services/UserClientService";
+import { getAllUsers } from '../../services/UserClientService';
 // components
-import TestPlayer from "../../components/TestPlayer/TestPlayer";
-import { useMainContext } from "../../components/Context/Context";
-import AddMembersSelect from "../../components/AddMembersSelect/AddMembersSelect";
-import AddMixtapeForm from "../../components/AddMixtapeForm/AddMixtapeForm";
+import TestPlayer from '../../components/TestPlayer/TestPlayer';
+import { useMainContext } from '../../components/Context/Context';
+import AddMembersSelect from '../../components/AddMembersSelect/AddMembersSelect';
+import AddMixtapeForm from '../../components/AddMixtapeForm/AddMixtapeForm';
 // styling
-import { MdPlayArrow } from "react-icons/md";
+import { MdPlayArrow } from 'react-icons/md';
 
 const Channel = () => {
   const { user } = useMainContext();
@@ -30,57 +30,74 @@ const Channel = () => {
     users: [],
   };
 
-  const [users, setUsers] = useState<User[]>(initialState.users)
+  const [users, setUsers] = useState<User[]>(initialState.users);
 
   useEffect(() => {
     async function retrieveAllUsers() {
       try {
         const allUsers = await getAllUsers();
-        setUsers(allUsers)
+        setUsers(allUsers);
       } catch (error) {
-        console.error('error getting all users')
+        console.error('error getting all users');
       }
     }
     setChannel(channel);
     retrieveAllUsers();
-  }, [channel])
-
+  }, [channel]);
 
   const toggleMixForm = () => {
-    setShowMixForm(!showMixForm)
-    setShowMemberForm(false)
-
-  }
+    setShowMixForm(!showMixForm);
+    setShowMemberForm(false);
+  };
   const toggleMemberForm = () => {
-    setShowMemberForm(!showMemberForm)
-    setShowMixForm(false)
-  }
+    setShowMemberForm(!showMemberForm);
+    setShowMixForm(false);
+  };
 
   return (
     <div id="channel">
-      <div id="channel-element" className="text-tapeWhite h-72 bg-gradient-to-r from-tapePink to-tapeYellow flex justify-between p-10 w-11/12 m-12 rounded-2xl" >
-        <div id="channel-info" >
-          <h1 className="flex items-center" ><MdPlayArrow size={35} />{channel.name}</h1>
-          <p className="pl-3" >{`${channel.mixTapes ? channel.mixTapes.length : 0} mixtape${channel.mixTapes.length > 1 ? 's' : ''}`}</p>
-          <p className="pl-3">{channel.members ? channel.members.length : 'no members'}</p>
+      <div
+        id="channel-element"
+        className="text-tapeWhite h-72 bg-gradient-to-r from-tapePink to-tapeYellow flex justify-between p-10 w-11/12 m-12 rounded-2xl"
+      >
+        <div id="channel-info">
+          <h1 className="flex items-center">
+            <MdPlayArrow size={35} />
+            {channel.name}
+          </h1>
+          <p className="pl-3">{`${
+            channel.mixTapes ? channel.mixTapes.length : 0
+          } mixtape${channel.mixTapes.length > 1 ? 's' : ''}`}</p>
+          <p className="pl-3">
+            {channel.members ? channel.members.length : 'no members'}
+          </p>
         </div>
         <img src={channel.picture} className="w-48 rounded-2xl object-cover" />
-
       </div>
-      <button onClick={toggleMixForm} className="white-button ml-12" >Add Mixtape</button>
-      <button onClick={toggleMemberForm} className="white-button" >Add Members</button>
-      {
-        showMixForm ? (
-         <AddMixtapeForm  channelId={channel._id} setChannel={setChannel} user={user} setShowMixForm={setShowMixForm}/> ) : (<></>)
-      }
-      {
-        showMemberForm ? (
-          <AddMembersSelect channelId={channel._id} setChannel={setChannel} />) : (<></>)
-      }
+      <button onClick={toggleMixForm} className="white-button ml-12">
+        Add Mixtape
+      </button>
+      <button onClick={toggleMemberForm} className="white-button">
+        Add Members
+      </button>
+      {showMixForm ? (
+        <AddMixtapeForm
+          channel={channel}
+          channelId={channel._id}
+          setChannel={setChannel}
+          setShowMixForm={setShowMixForm}
+        />
+      ) : (
+        <></>
+      )}
+      {showMemberForm ? (
+        <AddMembersSelect channelId={channel._id} setChannel={setChannel} />
+      ) : (
+        <></>
+      )}
       <TestPlayer />
     </div>
-  )
-
-}
+  );
+};
 
 export default Channel;
