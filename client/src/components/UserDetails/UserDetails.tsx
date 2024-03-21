ab9cc18c222c39c2a4fcd07dd18f682a91a513e5
JayCeeKay1991 <johanna.kveton@gmail.com>
2024-03-21 12:22:43 +0100

    fix: hotfix syntax userdetails

--- client/src/components/UserDetails/UserDetails.tsx
+++ client/src/components/UserDetails/UserDetails.tsx
@@ -1,44 +1,36 @@
-
 import React, { useState, MouseEventHandler, useEffect } from 'react';
 import "./UserDetails.css"
-
 import { useMainContext } from '../Context/Context';
-import { updateUser } from '@/services/UserClientService';
-import { User } from '@/types/User';
-import { postImageToCloudinary } from '@/services/CloudinaryService';
-
+import { updateUser } from '../../services/UserClientService';
+import { User } from '../../types/User';
+import { postImageToCloudinary } from '../../services/CloudinaryService';
 import johnMartin from '../AppNav/johnmartin.jpg'
 import { HiPlus } from "react-icons/hi2";
 import { GoPencil } from "react-icons/go";
-
-
 export type FormValuesUserProfile = {
   username: string;
   email: string;
   password: string;
   profilePic: string;
 };
-
 export default function UserDetails() {
   const { user, setUser } = useMainContext();
   const initialFormState = {
     username: user.userName,
     email: user.email,
-    password: '',
+    password: "",
     profilePic: user.profilePic,
   };
-  const [formValuesProfile, setFormValuesProfile] =
-    useState<FormValuesUserProfile>(initialFormState);
-
-  const [changeUsername, setChangeUsername] = useState(false);
-  const [changeEmail, setChangeEmail] = useState(false);
-  const [changePassword, setChangePassword] = useState(false);
-  const [changeProfilePic, setChangeProfilePic] = useState(false);
+  const [ formValuesProfile , setFormValuesProfile] = useState<FormValuesUserProfile>(initialFormState);
+  const [ changeUsername, setChangeUsername ] = useState(false);
+  const [ changeEmail, setChangeEmail ] = useState(false);
+  const [ changePassword, setChangePassword ] = useState(false);
+  const [ changeProfilePic, setChangeProfilePic ] = useState(false);
   const [formPictureFile, setFormPictureFile] = useState<File | null>(null);
-
   function handleEdit(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
-    switch (e.target) {
+    const target = e.currentTarget;
+    switch (target.id) {
       case "username":
         setChangeUsername(!changeUsername);
         break;
@@ -55,8 +47,6 @@ export default function UserDetails() {
               break;
      }
    }
-
-
   async function changeHandler(e: React.ChangeEvent<HTMLInputElement>) {
     const { name, value, type, files } = e.target;
     if (type === 'file' && files) {
@@ -88,9 +78,6 @@ export default function UserDetails() {
       }
       } else setFormValuesProfile({ ...formValuesProfile, [name]: value });
   }
-
-
-
   async function submitHandler(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     if (formValuesProfile.password !== user.password) {
@@ -102,11 +89,11 @@ export default function UserDetails() {
         profilePic: user.profilePic,
         channels: user.channels,
         mixTapes: user.mixTapes ? [...user.mixTapes] : [],
-      };
-      const updatedUser = await updateUser(newUser);
-      if (updatedUser) {
-        setUser(updatedUser);
       }
+       const updatedUser = await updateUser(newUser);
+       if (updatedUser) {
+         setUser(updatedUser);
+       }
     } else {
         const newUser: Omit<User, "password">  = {
           _id: user._id,
@@ -119,7 +106,6 @@ export default function UserDetails() {
       updateUser(newUser);
     }
          ///////////////////////////////////////////////
-         
          switch (e.target) {
            case document.getElementById("usernameForm"):
             setChangeUsername(!changeUsername);
@@ -183,129 +169,87 @@ export default function UserDetails() {
                   <button onClick={handleEdit} id='username' className='border-0 fontFamily-sans text-3xl'><b>{user.userName}</b></button>
               )}
             </div>
-          )}
+            </form>
+            <div className='flex flex-row'>
+      <div className='flex flex-col p-2 justify-center items-center'>
+          <div>{user.mixTapes.length}</div>
+          <div>Mixes</div>
         </div>
-      </form>
-      <form id='usernameForm' onSubmit={submitHandler}>
-        <div id='allUsername'>
-          <label>username:</label>
-          {changeUsername ? (
-            <div>
-              <input
-                name='username'
-                type='text'
-                onChange={changeHandler}
-                value={formValuesProfile.username}
-                required={true}
-              />
-              <button
-                className='submitButton'
-                onClick={handleEdit}
-                id='username'>
-                cancel
-              </button>
-              <button className='submitButton' type='submit'>
-                save
-              </button>
+        <div className='flex flex-col p-2 justify-center items-center'>
+          <div>{user.channels.length }</div>
+          <div>Channels</div>
+        </div>
+      </div>
+          <div id='emailAndPassword' className='flex flex-col'>
+            <form id="emailForm" onSubmit={submitHandler}>
+          <div id='allEmail' className='flex flex-col mb-10'>
+          <label className='block'><b>Email</b></label>
+              {changeEmail ? (
+                <div className='flex flex-col mb-10'>
+                 <input
+                   name="email"
+                   type="text"
+                   className=' border border-tapeGray rounded-md bg-tapeBlack  text-tapeDarkGrey'
+                   onChange={changeHandler}
+                   value={formValuesProfile.email}
+                   required={true}
+                 />
+                 <div className='flex flex-row'>
+                 <button className='rounded-full h-5 w-10 flex  p-1 pb-2 items-center ml-12 mr-5' onClick={handleEdit} id='email'>
+                   cancel
+                 </button>
+                  <button className='rounded-full h-5 w-10 flex p-1 pb-2 items-center bg-tapeGray ' type="submit">
+                    save
+                  </button>
+                 </div>
+                </div>
+              ) : (
+                <div className='flex flex-row space-x-10'>
+                  <p>{user.email}</p>
+                  <button onClick={handleEdit} id='email' className='border-0'><GoPencil /></button>
+                </div>
+              )}
             </div>
-          ) : (
-            <button onClick={handleEdit} id='username'>
-              {user.userName}
-            </button>
-          )}
-        </div>
-      </form>
-      <div>
-        <form id='emailForm' onSubmit={submitHandler}>
-          <div id='allEmail'>
-            <label>email:</label>
-            {changeEmail ? (
-              <div>
-                <input
-                  name='email'
-                  type='text'
-                  onChange={changeHandler}
-                  value={formValuesProfile.email}
-                  required={true}
-                />
-                <button
-                  className='submitButton'
-                  onClick={handleEdit}
-                  id='email'>
-                  cancel
-                </button>
-                <button className='submitButton' type='submit'>
-                  save
-                </button>
-              </div>
-            ) : (
-              <button onClick={handleEdit} id='email'>
-                {user.email}
-              </button>
-            )}
-          </div>
-        </form>
-
-        <form id='passwordForm' onSubmit={submitHandler}>
-          <div id='allPassword'>
-            <label>password:</label>
-            {changePassword ? (
-              <div>
-
+            </form>
+          <form id="passwordForm" onSubmit={submitHandler}>
+            <div id='allPassword'>
+              <label className='block'><b>Password</b></label>
+              {changePassword ? (
+                <div>
                 <input
                   name="password"
                   type="password"
                   className=' border border-tapeGray rounded-md bg-tapeBlack  text-tapeDarkGrey'
-
                   onChange={changeHandler}
                   value={formValuesProfile.password}
                 />
-                <button
-                  className='submitButton'
-                  onClick={handleEdit}
-                  id='password'>
-                  cancel
-                </button>
-                <button className='submitButton' type='submit'>
-                  save
-                </button>
+                <div className='flex flex-row'>
+                 <button className='rounded-full h-5 w-10 flex  p-1 pb-2 items-center ml-12 mr-5' onClick={handleEdit} id='password'>
+                   cancel
+                 </button>
+                <button className='rounded-full h-5 w-10 flex p-1 pb-2 items-center bg-tapeGray' type="submit">
+                    save
+                  </button>
+                </div>
+                </div>
+              ) : (
+                <div className='flex flex-row justify-between'>
+                <h2 className='text-3xl'>.........</h2>
+                <button onClick={handleEdit} id='password' className='border-0'><GoPencil /></button>
               </div>
-            ) : (
-              <button onClick={handleEdit} id='password'>
-                set new password
-              </button>
-            )}
-          </div>
-        </form>
-      </div>
-
-      <div>
-        <div>
-          Channels:
-          {user.channels.length > 0 ? (
-            user.channels!.map((channel) => (
-              <div>
-                <div>Channel name: {channel.name}</div>
-              </div>
-            ))
-          ) : (
-            <div>No channels yet</div>
-          )}
+              )}
+            </div>
+            </form>
         </div>
-        <div>
-          Tapes:
-          {user.mixTapes.length > 0 ? (
-            user.mixTapes.map((tape) => (
-              <div>
-                <div>Tape name: {tape.name}</div>
-              </div>
-            ))
-          ) : (
-            <div>No tapes yet</div>
-          )}
-        </div>
-      </div>
     </div>
   );
 }
+
+
+
+
+
+
+
+
 
