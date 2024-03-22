edd8d38367d6bcc6ae18797b331a47e3a0033e18
Alastair Fraser <alastairfraser.work@gmail.com>
2024-03-22 19:27:47 +0100

    hotfix comments

--- client/src/components/Comment/Comment.tsx
+++ client/src/components/Comment/Comment.tsx
@@ -1,30 +1,46 @@
 import { CommentsType } from "../../types/Comments";
-import placeholderImg from "../../assets/img/pexels-juan-gomez-2589650.jpg";
 import { TimePrettier } from "../../utils/timePrettifier";
+import { User } from "../../types/User";
+import { useMainContext } from "../Context/Context";
 
 type propsType = {
   comment: CommentsType;
+  user: User;
 };
 
 function Comment({ comment }: propsType) {
+  const { user, setUser } = useMainContext();
+
   return (
     <div className="bg-tapeBlack w-[700px] h-[120px] flex flex-row pl-[20px] rounded-[20px] p-[10px] mb-[50px] items-center border-tapeDarkGrey border-[1px]">
       <div
         id="comment-crop"
-        className="w-[80px] h-[80px] overflow-hidden rounded-full mr-[40px]"
+        className="w-[80px] h-[80px] overflow-hidden rounded-full mr-[40px] flex items-center justify-center"
       >
-        <img src={placeholderImg} className="w-full" alt="Profile" />
+        <img
+          src={user.profilePic}
+          className="w-full"
+          alt="get a profile pic you dummy"
+        />
       </div>
       <div className="flex flex-col leading-[30px] justify-start items-start">
         <div className=" flex flex-row">
-          <p className="bg-tape font-semibold text-[25px] mr-[20px]" data-testid='comment-author'>
+          <p
+            className="bg-tape font-semibold text-[25px] mr-[20px]"
+            data-testid="comment-author"
+          >
             {comment.owner?.userName || "Unknown User"}
           </p>
           <p className="font-medium text-[20px] text-tapeDarkGrey">
             {TimePrettier(comment.date)}
           </p>
         </div>
-        <p className="text-[25px] whitespace-nowrap" data-testid='comment-content'>{comment.message}</p>
+        <p
+          className="text-[25px] whitespace-nowrap"
+          data-testid="comment-content"
+        >
+          {comment.message}
+        </p>
       </div>
     </div>
   );
