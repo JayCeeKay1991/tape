16b5d20b1e9bb9edd29911a2617beb94e4fa31b3
Nadav <nadavberenstain@gmail.com>
2024-03-23 15:43:37 +0100

    notification backEnd work

--- server/src/models/mixTape.ts
+++ server/src/models/mixTape.ts
@@ -1,47 +1,62 @@
-import { Document, Types } from 'mongoose';
-import mongoose from '.';
-
+import { Document, Types } from "mongoose";
+import mongoose from ".";
+import NotificationModel from "./notifications";
+import { ChannelType } from "./channel";
+import ChannelModel from "./channel";
 type MixTapeDocument = Document<unknown, {}, MixTapeType> & MixTapeType;
 
 export type MixTapeType = {
   name: string;
   url: string;
   duration: number;
-  channel: Types.ObjectId;
+  channel: ChannelType;
   creator: Types.ObjectId;
 };
 
-// defining data structure
+// Defining data structure
 const MixTape = new mongoose.Schema<MixTapeType>({
   name: { type: String, required: true },
   url: { type: String, required: true },
   duration: { type: Number, required: true },
   channel: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Channel',
+    ref: "Channel",
     required: true,
   },
-
   creator: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+    ref: "User",
     required: true,
   },
 });
 
-// updating the channel and user models whenever a mixtape has been post
+// Updating the channel and user models whenever a mixtape has been posted
 async function updateChannelAndUser(doc: MixTapeDocument) {
-  const channelUpdate = await mongoose
-    .model('Channel')
-    .updateOne({ _id: doc.channel }, { $push: { mixTapes: doc } });
+  const channelDoc = await ChannelModel.findById(doc.channel);
+  if (!channelDoc) {
+    throw new Error("Channel not found");
+  }
+  // Push the mixtape to the channel's mixTapes array
+  await mongoose
+    .model("Channel")
+    .updateOne({ _id: doc.channel }, { $push: { mixTapes: doc._id } });
 
-  const userUpdate = await mongoose
-    .model('User')
-    .updateOne({ _id: doc.creator }, { $push: { mixTapes: doc } });
+  // Create a notification for the new mixtape
+  const newNotification = await NotificationModel.create({
+    message: `A new mixtape "${doc.name}" was added.`,
+    channelName: channelDoc.name,
+    unNotifiiedUsers: channelDoc.members,
+    date: new Date(),
+  });
+  const updatedChannel = await ChannelModel.findByIdAndUpdate(
+    channelDoc._id,
+    { $push: { notifications: newNotification._id } },
+    { new: true }
+  ).populate("notifications");
 }
 
-MixTape.post('save', updateChannelAndUser);
+MixTape.post("save", updateChannelAndUser);
 
-const MixTapeModel = mongoose.model('MixTape', MixTape);
+const MixTapeModel = mongoose.model("MixTape", MixTape);
 
 export default MixTapeModel;
