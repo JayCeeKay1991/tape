113671bd6cb6772e5537cf8d501cdfe5d418359b
asiye duman <asiyeduman@MacBook-Pro-von-asiye.local>
2024-03-23 13:11:50 +0100

    small correction on map friends channels

--- client/src/routes/Dash/Dash.tsx
+++ client/src/routes/Dash/Dash.tsx
@@ -5,13 +5,11 @@ import ChannelItem from '@/components/ChannelItem/ChannelItem';
 import { getChannelsUserMemberOf } from '@/services/ChannelClientService';
 import { ChannelType } from '@/types/Channel';
 
-
 export default function Dash() {
-  const { user, setChannels : setChannelList } = useMainContext();
+  const { user, setChannels: setChannelList } = useMainContext();
   const channelList = user.channels;
   const [showForm, setShowForm] = useState(false);
   const [channels, setChannels] = useState<ChannelType[]>([]);
-
 
   useEffect(() => {
     // get all users to populate dropdown
@@ -20,47 +18,55 @@ export default function Dash() {
       setChannels(allChannels);
     }
     getAllChannels();
-  },[user]);
+  }, [user]);
   const toggleAddForm = () => {
     setShowForm(!showForm);
   };
 
-
   return (
     <div
-      id='channel-list-wrap'
-      className='text-tapeWhite bg-tapeOffBlack flex-col w-11/12 mx-10 my-5 px-10 rounded-3xl'>
+      id="channel-list-wrap"
+      className="text-tapeWhite bg-tapeOffBlack flex-col w-11/12 mx-10 my-5 px-10 rounded-3xl"
+    >
       <div
-        id='channel-list-header'
-        className='flex justify-between items-center pt-5'>
-        <h2 className='text-[50px]'><b>Your channels</b></h2>
-        <div id='channel-list-controls'>
-          <button onClick={toggleAddForm} className='white-button'>
+        id="channel-list-header"
+        className="flex justify-between items-center pt-5"
+      >
+        <h2 className="text-[50px]">
+          <b>Your channels</b>
+        </h2>
+        <div id="channel-list-controls">
+          <button onClick={toggleAddForm} className="white-button">
             Add channel
           </button>
         </div>
       </div>
-      <div id='all-channels' className='flex flex-col gap-x-10 py-1'>
-      <p className='text-[20px] text-tapeDarkGrey'>{channelList.length} streams</p>
-      <div id='channel-list' className='flex gap-x-10 py-12'>
-        {channelList.length
-          ? channelList.map((channel) => (
-            <ChannelItem key={channel._id} channel={channel} />
-          ))
-          : 'No channels yet.'}
-      </div>
-      <h2 className='text-[50px]'><b>Friends channels</b></h2>
-      <p className='text-[20px] text-tapeDarkGrey'>{channels.length} streams</p>
-      <div id='membership-channels' className='flex gap-x-10 py-12'>
-       {channels.length > 0 ? (
-         channels.map(channel => (
-          channel.owner._id === user._id ? null : <ChannelItem key={channel._id} channel={channel} />
-         ))
-         ) : (
-        'No channels yet.'
-       )}
-      </div>
-
+      <div id="all-channels" className="flex flex-col gap-x-10 py-1">
+        <p className="text-[20px] text-tapeDarkGrey">
+          {channelList.length} streams
+        </p>
+        <div id="channel-list" className="flex gap-x-10 py-12">
+          {channelList.length
+            ? channelList.map((channel) => (
+                <ChannelItem key={channel._id} channel={channel} />
+              ))
+            : 'No channels yet.'}
+        </div>
+        <h2 className="text-[50px]">
+          <b>Friends channels</b>
+        </h2>
+        <p className="text-[20px] text-tapeDarkGrey">
+          {channels.length} streams
+        </p>
+        <div id="membership-channels" className="flex gap-x-10 py-12">
+          {channels.length > 0
+            ? channels.map((channel) =>
+                channel.owner.toString() === user._id ? null : (
+                  <ChannelItem key={channel._id} channel={channel} />
+                )
+              )
+            : 'No channels yet.'}
+        </div>
       </div>
       {showForm ? (
         <AddChannelForm
@@ -70,4 +76,4 @@ export default function Dash() {
       ) : null}
     </div>
   );
-};
+}
