b90431c9cde040f8e9c6099ad7185e655d702af4
Nadav <nadavberenstain@gmail.com>
2024-03-25 18:26:31 +0100

    notification update and delete backEnd is ready

--- server/src/controllers/notification/notification.ts
+++ server/src/controllers/notification/notification.ts
@@ -2,6 +2,7 @@ import NotificationModel from "../../models/notifications";
 import { NotificationType } from "../../models/notifications";
 import { Request, Response } from "express";
 import ChannelModel, { ChannelType } from "../../models/channel";
+import user from "../user/user";
 
 export async function createNotification(req: Request, res: Response) {
   try {
@@ -26,5 +27,55 @@ export async function createNotification(req: Request, res: Response) {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Could not create channel." });
+  }
+}
+
+export async function updateNotification(req: Request, res: Response) {
+  try {
+    const userId = req.params.id;
+    const { unNotifiedUsers, _id } = req.body;
+    if (!unNotifiedUsers) {
+      return res.status(400).json({
+        error: "400",
+        message: "Bad request: unNotifiedUsers is required.",
+      });
+    }
+
+    if (unNotifiedUsers.length === 1) {
+      deleteNotification(_id);
+      return res.status(200).json({
+        message: "Successfully deleted notification.",
+      });
+    } else if (unNotifiedUsers.length > 1) {
+      const UpdatedUnNotifiedUsersArray = unNotifiedUsers.filter(
+        (user: string) => {
+          return user !== req.params.id;
+        }
+      );
+      const updatedNotification = await NotificationModel.findOneAndUpdate(
+        { _id: userId },
+        { $set: { unNotifiedUsers: UpdatedUnNotifiedUsersArray } },
+        { new: true }
+      );
+      res.status(201).json(updatedNotification);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message:
+        "An unexpected error occurred while editing the user. Please try again later.",
+    });
+  }
+}
+
+async function deleteNotification(notificationId: string) {
+  try {
+    const deletedNotification = await NotificationModel.findOneAndDelete({
+      _id: notificationId,
+    });
+    return deletedNotification;
+  } catch (error) {
+    console.error(error);
+    return error;
   }
 }
