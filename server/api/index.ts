d9a1742e5143858f597e84c942c05a23bf92c168
JayCeeKay1991 <johanna.kveton@gmail.com>
2024-03-25 14:45:12 +0100

    fix: deploy

--- server/api/index.ts
+++ server/api/index.ts
@@ -12,8 +12,8 @@ app.use(express.json());
 
 app.use(router);
 
-// app.listen(PORT, () => {
-//   console.log(`Server listening on port ${PORT}`)
-// });
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+});
 
 export = app;
