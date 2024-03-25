f3057a5a8f1a9da07eb3c417cbf662421142a348
JayCeeKay1991 <johanna.kveton@gmail.com>
2024-03-25 12:15:33 +0100

    feat: added vercel.json

--- server/src/router.ts
+++ server/src/router.ts
@@ -18,6 +18,7 @@ import { createMixTape } from "./controllers/mixTape/mixTape";
 
 const router = express.Router();
 
+router.get("/", (req, res) => res.send('Hiiii'));
 router.get("/users", getAllUsers);
 
 router.get("/users/:userId", getUserById);
