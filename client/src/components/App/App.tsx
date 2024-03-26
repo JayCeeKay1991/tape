164e212a7c3c3077d6e4d105de8111686accc544
Joe Copplestone <strasseneck@gmail.com>
2024-03-26 16:07:46 +0100

    Feat: Fix player in new layout

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,15 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import { useMainContext } from '../Context/Context';
 import PlayerContextProvider from '../Context/PlayerContext'; // Renamed import
-import Player from '../Player/Player';
 
 const App = () => {
-
-  const { user } = useMainContext();
   return (
     <PlayerContextProvider>
       <div className="App">
-        {user._id ? <Player /> : <></>}
+        {/* <Player /> */}
         <Outlet />
       </div>
     </PlayerContextProvider>
