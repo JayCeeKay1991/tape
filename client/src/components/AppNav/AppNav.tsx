1158ae0c9822c4659a63ad0ccd53b83669bab4a8
JayCeeKay1991 <johanna.kveton@gmail.com>
2024-03-26 11:10:04 +0100

    feat: search tool

--- client/src/components/AppNav/AppNav.tsx
+++ client/src/components/AppNav/AppNav.tsx
@@ -2,15 +2,13 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useMainContext, initialStateUser } from '@/components/Context/Context';
 import { MdHome } from 'react-icons/md';
 import { FiLogOut } from 'react-icons/fi';
-import { useState } from 'react';
-import { IoSearch } from 'react-icons/io5';
 import { logout } from '@/services/UserClientService';
 import { usePlayerContext } from '../Context/PlayerContext';
 
 const AppNav = () => {
   const { user, setUser } = useMainContext();
   const {setCurrentStream} = usePlayerContext();
-  const [isSearchVisible, setIsSearchVisible] = useState(false);
+
 
   const navigate = useNavigate();
 
@@ -25,9 +23,6 @@ const AppNav = () => {
     }
   };
 
-  const toggleSearch = () => {
-    setIsSearchVisible(!isSearchVisible);
-  };
 
   return (
     <nav className="w-full h-20 bg-tapeBlack fixed top-0 left-0 flex flex-row items-center justify-between pr-[20px] pl-[20px]">
@@ -37,18 +32,6 @@ const AppNav = () => {
             <MdHome size={30} />
           </button>
         </Link>
-        <div id="searchWrap">
-          <button className="border-none mt-[2px]" onClick={toggleSearch}>
-            <IoSearch size={25} className="border-none" />
-          </button>
-          {isSearchVisible && (
-            <input
-              className="bg-tapeBlack text-tapeWhite mx-8 focus:outline none"
-              type="text"
-              placeholder="Search..."
-            />
-          )}
-        </div>
       </div>
       <div className=" w-[100px] pl-[20px] flex flex-row  bg-tapeWhite rounded-full items-center justify-between">
         <button
--- client/src/routes/Dash/Dash.tsx
+++ client/src/routes/Dash/Dash.tsx
@@ -1,21 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, ChangeEvent } from 'react';
 import { useMainContext } from '@/components/Context/Context';
 import AddChannelForm from '@/components/AddChannelForm/AddChannelForm';
 import ChannelItem from '@/components/ChannelItem/ChannelItem';
 import { getChannelsUserMemberOf } from '@/services/ChannelClientService';
 import { ChannelType } from '@/types/Channel';
+import { IoSearch } from 'react-icons/io5';
 
 export default function Dash() {
   const { user } = useMainContext();
   const channelList = user.channels;
   const [showForm, setShowForm] = useState(false);
+  const [search, setSearch] = useState('');
   const [channels, setChannels] = useState<ChannelType[]>([]);
+  const [filteredChannels, setFilteredChannels] = useState<ChannelType[]>([]);
+  const [filteredChannelsOwn, setFilteredChannelsOwn] = useState<ChannelType[]>([]);
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
+
 
   useEffect(() => {
-    // get all users to populate dropdown
     async function getAllChannels() {
       const allChannels = await getChannelsUserMemberOf(user._id);
       setChannels(allChannels);
+      setFilteredChannels(allChannels);
+      setFilteredChannelsOwn(channelList);
     }
     getAllChannels();
   }, [user]);
@@ -24,8 +31,52 @@ export default function Dash() {
     setShowForm(!showForm);
   };
 
+  const toggleSearch = () => {
+    setIsSearchVisible(!isSearchVisible);
+  };
+
+  const searchFilter = (e: ChangeEvent<HTMLInputElement>) => {
+    const searchValue = e.target.value.toLowerCase();
+    if (!searchValue.trim()) {
+      setFilteredChannels(channels);
+      setFilteredChannelsOwn(channelList);
+      setSearch('');
+    } else {
+      setSearch(searchValue);
+      const searchResult: ChannelType[] = channels.filter(channel => {
+        return Object.values(channel).some(value =>
+          value.toString().toLowerCase().startsWith(searchValue)
+        );
+      });
+      const searchResultOwn: ChannelType[] = channelList.filter(channel => {
+        return Object.values(channel).some(value =>
+          value.toString().toLowerCase().startsWith(searchValue)
+        );
+      });
+
+     setFilteredChannels(searchResult);
+     setFilteredChannelsOwn(searchResultOwn);
+    }
+
+  }
+
+
   return (
     <div id="dashWrapper" className="w-full h-full">
+      <div id="searchWrap" className='fixed top-[22px] left-[80px]' >
+        <button className="border-none mt-[2px]" onClick={toggleSearch}>
+          <IoSearch size={25} className="border-none" />
+        </button>
+        {isSearchVisible && (
+          <input
+            className="bg-tapeBlack text-tapeWhite mx-8 focus:outline none"
+            onChange={searchFilter}
+            value={search}
+            type="text"
+            placeholder="Search..."
+          />
+        )}
+      </div>
       <div
         id="channel-list-wrap"
         className="text-tapeWhite bg-tapeOffBlack flex-col w-11/12 mx-10 my-5 px-10 rounded-3xl mt-[80px]"
@@ -43,12 +94,12 @@ export default function Dash() {
 
         <div id="all-channels" className="flex flex-col gap-x-10 py-1">
           <p className="text-[20px] text-tapeDarkGrey">
-            {channelList.length} streams
+            {filteredChannelsOwn.length} streams
           </p>
           <div className="w-full h-[600px] overflow-scroll">
             <div id="channel-list" className="flex gap-x-10 py-12">
-              {channelList.length
-                ? channelList.map((channel) => (
+              {filteredChannelsOwn.length
+                ? filteredChannelsOwn.map((channel) => (
                     <ChannelItem key={channel._id} channel={channel} />
                   ))
                 : "No channels yet."}
@@ -64,8 +115,8 @@ export default function Dash() {
 
           <div className="w-full h-[600px] overflow-scroll">
             <div id="membership-channels" className="flex gap-x-10 py-12">
-              {channels.length > 0
-                ? channels.map((channel) =>
+              {filteredChannels.length > 0
+                ? filteredChannels.map((channel) =>
                     channel.owner.toString() === user._id ? null : (
                       <ChannelItem key={channel._id} channel={channel} />
                     )
