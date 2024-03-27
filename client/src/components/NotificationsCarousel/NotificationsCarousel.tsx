2fd81d216929aa361e0e30f41e7e6a35968e143f
Joe Copplestone <strasseneck@gmail.com>
2024-03-27 17:52:06 +0100

    Fix: timer on notifications

--- client/src/components/NotificationsCarousel/NotificationsCarousel.tsx
+++ client/src/components/NotificationsCarousel/NotificationsCarousel.tsx
@@ -1,40 +1,39 @@
-import { useEffect, useState } from "react";
-import { NotificationType } from "@/types/Notification"
+import { useState, useEffect } from "react";
+import { NotificationType } from "@/types/Notification";
 
 interface NotificationsCarouselProps {
     notifications: NotificationType[];
 }
 
 const NotificationsCarousel = ({ notifications }: NotificationsCarouselProps) => {
-    const [currentNotification, setCurrentNotification] = useState<NotificationType>(notifications[0])
-    const [index, setIndex] = useState<number>(0)
+    const [currentNotification, setCurrentNotification] = useState<NotificationType>(notifications[0]);
+    const [index, setIndex] = useState<number>(0);
 
-
-    const updateCarousel = () => {
+    useEffect(() => {
         const timerId = setInterval(() => {
             if (index === notifications.length - 1) {
                 setCurrentNotification(notifications[0]);
-                setIndex(0)
+                setIndex(0);
+            } else {
+                const newIndex = index + 1;
+                setCurrentNotification(notifications[newIndex]);
+                setIndex(newIndex);
             }
-            const newIndex = index + 1;
-            setCurrentNotification(notifications[newIndex]);
-            setIndex(newIndex)
         }, 10000);
-        return () => clearInterval(timerId);
-    }
-    updateCarousel()
+        return () => clearInterval(timerId); 
+    }, [index, notifications]); 
 
-    console.log(currentNotification)
+    console.log(notifications)
+
     return (
         <div className="carousel-wrapper bg-gradient-to-b from-tapePink to-tapeYellow rounded-3xl w-full h-[250px] p-[30px]">
             <h1 className="text-tapeWhite text-7xl">What's new</h1>
             <div id="carousel-item" className="flex justify-center">
-                <p className="text-2xl">{currentNotification!.message}</p>
+                <p className="text-2xl">{currentNotification.message}</p>
                 <h1></h1>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default NotificationsCarousel
+export default NotificationsCarousel;
--- server/src/controllers/mixTape/mixTape.ts
+++ server/src/controllers/mixTape/mixTape.ts
@@ -20,7 +20,7 @@ export const createMixTape = async (req: Request, res: Response) => {
     res.status(201).json(savedMixTape);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Could not create mixtape." });
+    res.status(500).json({ message: `Could not create mixtape ${error}` });
   }
 };
 
