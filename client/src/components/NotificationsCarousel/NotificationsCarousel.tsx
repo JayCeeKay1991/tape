a32c5639430217160dbfc7d47466f71b2c65c350
Joe Copplestone <strasseneck@gmail.com>
2024-03-27 17:13:31 +0100

    Feat: Notification carousel

--- client/src/components/NotificationsCarousel/NotificationsCarousel.tsx
+++ client/src/components/NotificationsCarousel/NotificationsCarousel.tsx
@@ -9,28 +9,28 @@ const NotificationsCarousel = ({ notifications }: NotificationsCarouselProps) =>
     const [currentNotification, setCurrentNotification] = useState<NotificationType>(notifications[0])
     const [index, setIndex] = useState<number>(0)
 
-    useEffect(() => {
-        updateCarousel()
-    }, [])
 
     const updateCarousel = () => {
         const timerId = setInterval(() => {
-               if (index === notifications.length-1) {
+            if (index === notifications.length - 1) {
                 setCurrentNotification(notifications[0]);
                 setIndex(0)
-               }
+            }
             const newIndex = index + 1;
             setCurrentNotification(notifications[newIndex]);
             setIndex(newIndex)
-        }, 8000);
+        }, 10000);
         return () => clearInterval(timerId);
     }
+    updateCarousel()
 
     console.log(currentNotification)
     return (
-        <div className="carousel-wrapper bg-gradient-to-b from-tapePink to-tapeYellow rounded-3xl w-full h-[200px] flex justify-center">
-            <div id="carousel-item">
-                <p>{currentNotification!.message}</p>
+        <div className="carousel-wrapper bg-gradient-to-b from-tapePink to-tapeYellow rounded-3xl w-full h-[250px] p-[30px]">
+            <h1 className="text-tapeWhite text-7xl">What's new</h1>
+            <div id="carousel-item" className="flex justify-center">
+                <p className="text-2xl">{currentNotification!.message}</p>
+                <h1></h1>
             </div>
 
         </div>
