2f6f4d856c1f0cd72fe2ee68e44d5b55192a3e64
Alastair Fraser <alastairfraser.work@gmail.com>
2024-03-28 12:42:05 +0100

    style fix

--- client/src/components/Loading/Loading.tsx
+++ client/src/components/Loading/Loading.tsx
@@ -1,31 +1,29 @@
-import animationData from '../../assets/animations/wave.json'
+import animationData from '../../assets/animations/wave-white.json'
 import { motion, AnimatePresence } from 'framer-motion';
 import { useLottie } from 'lottie-react';
 const Loading = () => {
 
     const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: animationData,
-        rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice",
-        },
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice",
+      },
     };
 
     const { View } = useLottie(defaultOptions)
 
     return (
-        <AnimatePresence>
-            <motion.div
-                className="loading"
-                initial={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-            >
-                <div className="load-wrap">
-                    { View }
-                </div>
-            </motion.div>
-        </AnimatePresence>
+      <AnimatePresence>
+        <motion.div
+          className="loading w-full h-full flex flex-row justify-center items-center"
+          initial={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <div className="load-wrap  w-4/12 h-4/12">{View}</div>
+        </motion.div>
+      </AnimatePresence>
     );
 };
 
